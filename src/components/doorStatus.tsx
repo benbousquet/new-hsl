e2e671954e66352635104081bb2c12d0e575fc74
benbousquet <bbousque@asu.edu>
2024-08-11 14:26:01 -0700

    add door status unmount suggestion

--- src/components/doorStatus.tsx
+++ src/components/doorStatus.tsx
@@ -5,16 +5,32 @@ export default function DoorStatus() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    const controller = new AbortController(); // Create an instance of AbortController
+    const signal = controller.signal; // Get the signal to pass to fetch
+
     async function fetchData() {
       setIsLoading(true);
-      const res = await fetch(
-        "https://members.heatsynclabs.org/space_api.json"
-      );
-      const json = await res.json();
-      setIsDoorOpen(json.open);
-      setIsLoading(false);
+      try {
+        const res = await fetch(
+          "https://members.heatsynclabs.org/space_api.json",
+          { signal } // Pass the signal to the fetch call
+        );
+        const json = await res.json();
+        setIsDoorOpen(json.open);
+      } catch (error) {
+        if (error instanceof Error && error.name !== 'AbortError') { // Handle errors other than abort
+          console.error("Failed to fetch door status:", error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
+
     fetchData();
+
+    return () => {
+      controller.abort(); // Abort the fetch operation on component unmount
+    };
   }, []);
 
   if (isLoading) {
