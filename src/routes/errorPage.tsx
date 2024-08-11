efd079590924e7736580612c0c198516bdd8326b
benbousquet <bbousque@asu.edu>
2024-08-11 14:16:14 -0700

    change route error page

--- src/routes/errorPage.tsx
+++ src/routes/errorPage.tsx
@@ -1,12 +1,13 @@
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
+    // could not find type
+    const error: any = useRouteError();
     console.error(error);
     return (
         <div className="flex flex-col items-center">
-            <h1 className="text-3xl p-5">Error occurred...</h1>
-            <p>{JSON.stringify(error)}</p>
+            <h1 className="text-3xl p-5">Looks like something went wrong...</h1>
+            <h2 className="text-2xl">{`${error.status} ${error.statusText}`}</h2>
         </div>
     )
 }
